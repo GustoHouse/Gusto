be7660e21e162bdb60aa257e97ae80f079308ff3
Carl Michel <carlfmichel@gmail.com>
2019-03-18 22:42:29 -0400

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './gusto-logo.svg';
+import apexScreenshot from './apex-project-screenshot.png';
 import './App.css';
 
 class App extends Component {
@@ -8,18 +9,32 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="container">
+          <div className="intro">
+            <div className="left">
+              <span className="tag">Nuture Fascination.</span>
+            </div>
+            <div className="right">
+              <h1>Digital Builders.</h1>
+              <p>Gusto. Such a little word. But to live its true meaning could move mountains. It stands for vigor. It stands for curiosity. It’s the very thing that gives life genuine super powers.</p>
+              <p>Building things is our passion. Through websites and applications we tell stories and enrich lives.</p>
+            </div>
+          </div>
+          <div className="projects">
+            <div className="project">
+              <div className="image">
+                <img src={apexScreenshot} alt="Apex Tracker Screenshot" />
+              </div>
+              <div className="text">
+                <span className="tag">Application</span>
+                <h2>Apex Tracker</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut odio sit amet lorem rutrum finibus.</p>
+                <a href="#">Explore.</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
