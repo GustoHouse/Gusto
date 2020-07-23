677491f2e60c6b9b13303fbd72d596cc77e9e2d0
Ty Goss <>
2020-07-22 22:11:58 -0400

    testimonials

--- src/Components/Testimonials/testimonials.js
+++ src/Components/Testimonials/testimonials.js
@@ -3,11 +3,49 @@ import Testimony from './testimony';
 import './testimonials.scss';
 
 const gainesway = {
-    text: "",
-    name: "",
-    business: "",
-    color: ""
+  text: [
+    "Ty and Carl distilled everything we wanted to achieve with our website redesign into a clear vision and plan.",
+    "They considered the nuances of our unique business and built creative, elegant solutions that improved how our clients experience and interact with the site.",
+    "The analytics tell us the new site is a huge success, and the compliments keep rolling in from inside and outside our industry."
+  ],
+  name: "Ian Tapp",
+  business: "Gainesway Farms",
+  color: "#FF0255"
 };
+
+const healthcarousel = {
+  text: [
+    "We’ve been working with Ty & Carl at Gusto for over two years. In that time, they have become like an extension of our in-house marketing team.",
+    "They have helped us reduce redundancies in our web development architecture, which helped us save money on extensions we had used on our sites in the past.",
+    "When we need something done, they are always quick to help and can adapt to any project. They go above and beyond to find solutions to our problems, and are always happy to share how they got to their solution so that we can be better educated as a team going forward.",
+    "Their help has been invaluable over the years and I would not hesitate to recommend them to others!"
+  ],
+  name: "Kelly Beckstedt",
+  business: "Health Carousel",
+  color: "#FBEA40"
+};
+
+const orr = {
+  text: [
+    "I found Carl and Ty easy to work with and appreciated the way the business relationship felt like a partnership geared towards win/win solutions.",
+    "They provided high quality work at a fair price (and the end result was a big hit with our customers and internal stakeholders)."
+  ],
+  name: "Phil Aldridge",
+  business: "ORR Safety",
+  color: "#2DDAD3"
+};
+
+const zimmer = {
+  text: [
+    "Tyler and Carl are the unicorns of developers combining a cornucopia of highly sought-after qualities and skills including but not limited to: exceptional creativity, the utmost professionalism, exemplary communication, and thoughtful guidance.",
+    "Whatever wild idea we threw at them they took to heart, digested and figured out a crazy cool way to implement into the project.",
+    "Not only were they a delight to work alongside, we felt we had found true collaborators who were game to push the boundaries of what was possible all the while shepherding our project to completion with our highest satisfaction in mind.",
+    "We would jump at the chance for future collaborations."
+  ],
+  name: "Jim and Jess Zimmer",
+  business: "Zimmer Design",
+  color: "#FF0255"
+}
 
 class Testimonials extends Component {
   render() {
@@ -20,6 +58,12 @@ class Testimonials extends Component {
         </h2>
 
         <Testimony content={gainesway} />
+
+        <Testimony content={healthcarousel} />
+
+        <Testimony content={orr} />
+
+        <Testimony content={zimmer} />
 
       </section>
     );
--- src/Components/Testimonials/testimony.js
+++ src/Components/Testimonials/testimony.js
@@ -11,11 +11,15 @@ class Testimony extends Component {
             <path d="M0 68L10.5 37H17.5L5 73L0 68Z" fill="#5980F7"/>
             <path d="M77 0.5L82 5.5L50.5 37L45.5 32V0.5H77Z" fill="#5980F7"/>
             <path d="M44 68L54.5 37H61.5L49 73L44 68Z" fill="#5980F7"/>
-            <path d="M82.5906 33.6999L64.2906 73H48.9906L61.5906 37L50.7906 37L50.7906 5.19996L82.5906 5.19996V33.6999ZM38.1906 33.6999L19.8906 73H4.59058L17.1906 37L6.39058 37L6.39059 5.19995L38.1906 5.19995V33.6999Z" fill="#FBEA40"/>
+          <path d="M82.5906 33.6999L64.2906 73H48.9906L61.5906 37L50.7906 37L50.7906 5.19996L82.5906 5.19996V33.6999ZM38.1906 33.6999L19.8906 73H4.59058L17.1906 37L6.39058 37L6.39059 5.19995L38.1906 5.19995V33.6999Z" fill={this.props.content.color}/>
         </svg>
 
-        <strong>Ian Tapp</strong>
-        <span>Gainesway Farm</span>
+        {this.props.content.text.map((paragraph) =>
+          <p>{paragraph}</p>
+        )}
+
+        <strong>{this.props.content.name}</strong>
+        <span>{this.props.content.business}</span>
 
       </div>
     );
