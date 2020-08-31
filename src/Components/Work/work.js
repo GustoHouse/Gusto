bb5876c01c1456c980d3bb3df7a8494f3b9e3ef2
Ty Goss <tygoss@gmail.com>
2020-08-30 22:33:41 -0400

    Update work.js

--- src/Components/Work/work.js
+++ src/Components/Work/work.js
@@ -22,7 +22,7 @@ class Work extends Component {
             <div className="jobs">
               <Job name="Gainesway" image={Gainesway} url="https://www.gainesway.com" />
               <Job name="Cognision" image={Cognision} url="https://www.cognision.com" />
-              <Job name="Match Series" image={MatchSeries} url="https://www.matchseries.com" />
+              <Job name="Match Series" image={MatchSeries} url="http://www.matchseries.com" />
               <Job name="Setzer" image={Setzer} url="http://setzerproperties.com/" />
               <Job name="Zimmer Design" image={Zimmer} url="https://zimmer-design.com/" />
               <Job name="Holiday Inn" image={Holiday} url="http://hihurstbourne.com/" />
